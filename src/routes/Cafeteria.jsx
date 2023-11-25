831704c8081c9e5f0203cbc75add84d83fbde7e0
seolcu <seolcu0112@proton.me>
2023-11-25 14:54:24 +0900

    메뉴 탭 따로 추가

--- src/routes/Cafeteria.jsx
+++ src/routes/Cafeteria.jsx
@@ -12,6 +12,9 @@ export default function Cafeteria() {
     (singleCafeteriaInfo) => singleCafeteriaInfo.id == params.cafeteriaID
   );
 
+  const crowdedIndicatorStyle =
+    "text-center p-4 rounded font-medium text-base ";
+
   if (!cafeteriaInfo) {
     return <ErrorPage />;
   } else {
@@ -34,37 +37,27 @@ export default function Cafeteria() {
           src={getImageUrl(cafeteriaInfo.pictureName)}
           alt="사진"
         />
+
         <div className="grid grid-cols-2 gap-4">
-          <div
-            className={
-              "flex flex-col gap-4 justify-between items-center p-4 rounded font-medium " +
-              (cafeteriaInfo.crowded == 0
-                ? "bg-overlay0 text-base"
-                : cafeteriaInfo.crowded == 1
-                ? "bg-green text-base "
-                : cafeteriaInfo.crowded == 2
-                ? "bg-yellow text-base "
-                : cafeteriaInfo.crowded == 3
-                ? "bg-red text-base"
-                : "bg-blue text-base")
-            }
-          >
-            {cafeteriaInfo.crowded == 0
-              ? "휴무"
-              : cafeteriaInfo.crowded == 1
-              ? "여유"
-              : cafeteriaInfo.crowded == 2
-              ? "보통"
-              : cafeteriaInfo.crowded == 3
-              ? "혼잡"
-              : "정보 없음"}
+          {cafeteriaInfo.crowded == 0 ? (
+            <div className={crowdedIndicatorStyle + "bg-overlay0"}>휴무</div>
+          ) : cafeteriaInfo.crowded == 1 ? (
+            <div className={crowdedIndicatorStyle + "bg-green"}>여유</div>
+          ) : cafeteriaInfo.crowded == 2 ? (
+            <div className={crowdedIndicatorStyle + "bg-yellow"}>보통</div>
+          ) : cafeteriaInfo.crowded == 3 ? (
+            <div className={crowdedIndicatorStyle + "bg-red"}>혼잡</div>
+          ) : (
+            <div className={crowdedIndicatorStyle + "bg-blue"}>정보 없음</div>
+          )}
+
+          <div className="bg-base text-center p-4 rounded font-medium">
+            뭐 넣을지 고민중
           </div>
-          <Link
-            to={"/cafeteria/" + cafeteriaInfo.id + "/menu"}
-            className="bg-base flex flex-col gap-4 justify-between items-center p-4 rounded font-medium "
-          >
-            메뉴
-          </Link>
+        </div>
+
+        <div className="bg-base p-4 rounded flex flex-col gap-4">
+          <h2 className="text-lg font-medium">메뉴</h2>
         </div>
       </main>
     );
