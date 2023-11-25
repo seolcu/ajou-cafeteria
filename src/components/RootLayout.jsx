e706af23cab8a7b216326c8375ada83de3e7d65c
seolcu <seolcu0112@proton.me>
2023-11-25 14:20:21 +0900

    hotfix: bg color fixed

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
-    <div className="latte dark:frappe bg-crust text-text h-screen w-screen">
-      <div className="bg-mantle h-screen max-w-md mx-auto">{children}</div>
+    <div className="latte dark:frappe bg-crust text-text min-h-screen w-screen">
+      <div className="bg-mantle min-h-screen max-w-md mx-auto">{children}</div>
     </div>
   );
 }
