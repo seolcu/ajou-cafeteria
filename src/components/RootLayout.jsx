787c1673789eb552caf8e255d91c5f9acccbc9dc
seolcu <seolcu0112@proton.me>
2023-12-01 06:18:59 +0900

    new-feature: 다크모드 설정가능

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,6 +1,18 @@
+import useLocalStorage from "../functions/useLocalStorage";
+
 export default function RootLayout({ children }) {
+  const [theme, setTheme] = useLocalStorage("theme", "system");
   return (
-    <div className="latte dark:frappe bg-crust text-text min-h-screen w-screen">
+    <div
+      className={
+        "bg-crust text-text min-h-screen w-screen " +
+        (theme == "light"
+          ? "latte"
+          : theme == "dark"
+          ? "frappe"
+          : "latte dark:frappe")
+      }
+    >
       <div className="bg-mantle min-h-screen max-w-md mx-auto">{children}</div>
     </div>
   );
--- src/routes/settings.jsx
+++ src/routes/settings.jsx
@@ -6,6 +6,8 @@ import { ArrowUturnLeftIcon } from "@heroicons/react/20/solid";
 
 export default function Settings() {
   const [language, setLanguage] = useLocalStorage("language", "ko");
+  const [theme, setTheme] = useLocalStorage("theme", "system");
+  const [darkFlavor, setDarkFlavor] = useLocalStorage("darkFlavor", "frappe");
 
   return (
     <main className="p-4 flex flex-col gap-4">
@@ -38,6 +40,30 @@ export default function Settings() {
             onClick={() => setLanguage("en")}
           >
             English
+          </SimpleButton>
+        </div>
+      </div>
+
+      <div className="bg-base p-4 flex flex-col gap-4 rounded">
+        <h2 className="text-lg font-bold">테마 (새로고침 시 적용)</h2>
+        <div className="flex flex-row gap-4">
+          <SimpleButton
+            enableCondition={theme == "system"}
+            onClick={() => setTheme("system")}
+          >
+            시스템
+          </SimpleButton>
+          <SimpleButton
+            enableCondition={theme == "light"}
+            onClick={() => setTheme("light")}
+          >
+            라이트
+          </SimpleButton>
+          <SimpleButton
+            enableCondition={theme == "dark"}
+            onClick={() => setTheme("dark")}
+          >
+            다크
           </SimpleButton>
         </div>
       </div>
