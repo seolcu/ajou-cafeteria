119641fb603e8e6b938e2a29c8d75ce2308ea95a
seolcu <seolcu0112@proton.me>
2023-12-01 12:11:37 +0900

    new-feature: 오늘의 xx -> xx 메뉴

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -23,13 +23,12 @@ export default function Random({ cafeteriaInfo }) {
         >
           <div className="basis-1/2 flex flex-col gap-1">
             <h3 className="font-medium">
-              오늘의{" "}
               {meal.position == "breakfast"
-                ? "아침"
+                ? "아침 메뉴"
                 : meal.position == "lunch"
-                ? "점심"
+                ? "점심 메뉴"
                 : meal.position == "dinner"
-                ? "저녁"
+                ? "저녁 메뉴"
                 : "에러: 개발자에게 문의하세요"}
             </h3>
             <ul className="text-sm">
