f7bea6e814685babbfa4ac13948bb40e023c09f5
seolcu <seolcu0112@proton.me>
2023-12-01 12:55:27 +0900

    new-feature: 설정에 알레르기 키워드 등록 가능하게 함

--- src/routes/settings.jsx
+++ src/routes/settings.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import SimpleCheckBox from "../components/SimpleCheckbox";
 import SimpleButton from "../components/SimpleButton";
 import useLocalStorage from "../functions/useLocalStorage";
-import { ArrowUturnLeftIcon } from "@heroicons/react/20/solid";
+import { ArrowUturnLeftIcon, TrashIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 export default function Settings() {
   const [language, setLanguage] = useLocalStorage("language", "ko");
   const [theme, setTheme] = useLocalStorage("theme", "system");
 
+  const [allergyKeywords, setAllergyKeywords] = useLocalStorage(
+    "allergyKeywords",
+    []
+  );
+  const [allergyKeywordInput, setAllergyKeywordInput] = useState("");
   return (
     <main className="p-4 flex flex-col gap-4">
       <header className="flex flex-row gap-4 items-center">
@@ -71,6 +77,46 @@ export default function Settings() {
             다크
           </SimpleButton>
         </div>
+      </div>
+
+      <div className="bg-base p-4 flex flex-col gap-4 rounded-lg">
+        <h2 className="text-lg font-bold">알레르기 키워드</h2>
+        <div className="flex flex-row gap-4 items-center">
+          <input
+            type="text"
+            placeholder="알레르기 키워드를 입력하세요."
+            className="border-2 rounded-lg bg-surface0 border-text p-2 grow"
+            value={allergyKeywordInput}
+            onChange={(e) => setAllergyKeywordInput(e.target.value)}
+          />
+          <SimpleButton
+            enableCondition={allergyKeywordInput != ""}
+            onClick={() => {
+              setAllergyKeywords([...allergyKeywords, allergyKeywordInput]);
+              setAllergyKeywordInput("");
+            }}
+          >
+            추가
+          </SimpleButton>
+        </div>
+        <ul className="flex flex-col gap-2">
+          {allergyKeywords.map((allergyKeyword) => (
+            <li
+              className="rounded-lg p-2 bg-surface0 flex flex-row"
+              key={Math.random()}
+            >
+              <div className="grow truncate">{allergyKeyword}</div>
+              <TrashIcon
+                className="w-6 h-6 shrink-0"
+                onClick={() =>
+                  setAllergyKeywords(
+                    allergyKeywords.filter((k) => k != allergyKeyword)
+                  )
+                }
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
