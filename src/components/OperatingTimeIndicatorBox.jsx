35e4b7fd646e7238e85a1f0498a4e4c6706f2446
seolcu <seolcu0112@proton.me>
2023-12-03 14:25:59 +0900

    new-feature: changed text size of OperatingTimeIndicatorBox for better look

--- src/components/OperatingTimeIndicatorBox.jsx
+++ src/components/OperatingTimeIndicatorBox.jsx
@@ -1,7 +1,7 @@
 export default function OperatingTimeIndicatorBox({ operatingTime }) {
   return (
     <div className="bg-base p-3 flex flex-col gap-4 rounded-lg justify-center">
-      <ul className="text-center font-medium text-sm">
+      <ul className="text-center text-xs">
         {operatingTime.map((period) => {
           return <li key={Math.random()}>{period}</li>;
         })}
